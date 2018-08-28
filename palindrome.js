34330212f65302afb650780bcfb94e269f054dd0
Jeremy Hurley <jdhurley@gmail.com>
2018-08-28 09:51:49 -0700

    moved directory

---
+++ palindrome.js
@@ -0,0 +1,36 @@
+
+//Modifying native object
+//Adds `reverse` to all strings
+String.prototype.reverse = function reverse() {
+  return Array.from(this).reverse().join("");
+}
+
+
+//Defines a Phrase object
+
+function Phrase(content) {
+  this.content = content;
+
+// Returns content processed for palindrome testing
+  this.processedContent = function processedContent() {
+    return this.content.toLowerCase();
+}
+
+  //Returns true for a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+    return this.processedContent() === this.processedContent().reverse();
+  }
+}
+
+// //Defines a TranslatedPhrase object
+// function TranslatedPhrase(content, translation) {
+//   this.content = content;
+//   this.translation = translation;
+//
+//   // Returns content processed for palindrome testing
+//     this.processedContent = function processedContent() {
+//       return this.translation.toLowerCase();
+//   }
+// }
+//
+// TranslatedPhrase.prototype = new Phrase();
